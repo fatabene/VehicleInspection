173b6962ae37a6ed5f2f050a0b7fe1869b0ddee5
fatabene <94798155+fatabene@users.noreply.github.com>
2023-05-09 11:45:40 +0200

    Update build.php

--- build.php
+++ build.php
@@ -31,7 +31,7 @@ if ($conn->connect_errno) {
     
 
 
-    $daysOfWeek = array('Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday(closed)', 'Sunday(closed)', 'Monday');
+    $daysOfWeek = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday(closed)', 'Sunday(closed)');
 
     $firstDayOfMonth = mktime(0, 0, 0, (int)$month, 1, $year);
     $numberDays = date('t', $firstDayOfMonth);
