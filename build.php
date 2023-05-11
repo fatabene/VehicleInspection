493b84b40d0aee936abb9f39071dcb92c2395fcf
fatabene <94798155+fatabene@users.noreply.github.com>
2023-05-11 20:54:18 +0200

    Update build.php

--- build.php
+++ build.php
@@ -1,20 +1,6 @@
 <?php
 session_start();
 
-function build_calendar($month, $year)
-{
-    $host = 'containers-us-west-145.railway.app';
-    $port = '7013';
-    $user = 'root';
-    $password = '3tJjuqr3nLekRPJu1thG';
-    $database = 'railway';
-
-    $conn = new mysqli($host, $user, $password, $database, $port);
-
-    if ($conn->connect_errno) {
-        echo "Failed to connect to MySQL: " . $conn->connect_error;
-        exit();
-    }
 
     // Check if the user is logged in
     $isLoggedIn = isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] === true;
@@ -71,6 +57,22 @@ function build_calendar($month, $year)
             <button type='submit' name='logout' class='btn btn-primary'>Logout</button>
         </form>";
 
+    function build_calendar($month, $year)
+{
+ $host = 'containers-us-west-145.railway.app';
+$port = '7013';
+$user = 'root';
+$password = '3tJjuqr3nLekRPJu1thG';
+$database = 'railway';
+
+$conn = new mysqli($host, $user, $password, $database, $port);
+
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
+    exit();
+}
+
+
     $stmt = $conn->prepare("SELECT * FROM vehicleinspection_bookings_record WHERE MONTH(DATE) = ? AND YEAR(DATE) = ?");
     $stmt->bind_param('ss', $month, $year);
     $bookings = array();
@@ -84,13 +86,14 @@ function build_calendar($month, $year)
             $stmt->close();
         }
     }
+    
+
 
     $daysOfWeek = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday(closed)', 'Sunday(closed)');
     $month = date('m');
     $year = date('Y');
 
-    $firstDayOfMonth = mktime(0, 0, 0,
-    $firstDayOfMonth = mktime(0, 0, 0, (int)$month, 1, $year));
+    $firstDayOfMonth = mktime(0, 0, 0, (int)$month, 1, $year);
     $numberDays = date('t', $firstDayOfMonth);
     $dateComponents = getdate($firstDayOfMonth);
     $monthName = $dateComponents['month'];
@@ -100,11 +103,12 @@ function build_calendar($month, $year)
     $calendar .= "<center><h2 class='mb-4'>Open Monday to Friday 9am - 5pm</h2>";
     $calendar .= "<center><a href='my_bookings.php' class='btn btn-primary'>Mina bokningar</a>";
     $calendar .= "<center><h2 class='mb-4'></h2>";
-    $calendar .= "<a class='btn btn-xs btn-primary' href='?month=" . date('m', mktime(0, 0, 0, (int)$month - 1, 1, $year)) . "&year=" . date('Y', mktime(0, 0, 0, (int)$month - 1, 1, $year)) . "'>Previous Month</a> ";
+    $calendar .= "<a class='btn btn-xs btn-primary' href='?month=" . date('m', mktime(0, 0, 0, (int)$month - 1, 1, $year)) . "&year=" . date('Y', mktime(0, 0, 0,  (int)$month - 1, 1, $year)) . "'>Previous Month</a> ";
     $calendar .= " <a class='btn btn-xs btn-success' href='?month=" . date('m') . "&year=" . date('Y') . "'>Current Month</a> ";
-    $calendar .= "<a class='btn btn-xs btn-primary' href='?month=" . date('m', mktime(0, 0, 0, (int)$month + 1, 1, $year)) . "&year=" . date('Y', mktime(0, 0, 0, (int)$month + 1, 1, $year)) . "'>Next Month</a></center><br>";
+    $calendar .= "<a class='btn btn-xs btn-primary' href='?month=" . date('m', mktime(0, 0, 0,  (int)$month + 1, 1, $year)) . "&year=" . date('Y', mktime(0, 0, 0,  (int)$month + 1, 1, $year)) . "'>Next Month</a></center><br>";
     //$calendar .= "<center><a href='my_bookings.php' class='btn btn-primary'>Mina bokningar</a>";
     $calendar .= "<left><h3 class='mb-4'>$monthName $year</h3>";
+
 
     $calendar .= "<tr>";
     foreach ($daysOfWeek as $day) {
@@ -114,6 +118,7 @@ function build_calendar($month, $year)
     $currentDay = 1;
     $calendar .= "</tr><tr>";
 
+
     if ($dayOfWeek > 0) {
         for ($k = 0; $k < $dayOfWeek; $k++) {
             $calendar .= "<td  class='empty'></td>";
@@ -122,9 +127,11 @@ function build_calendar($month, $year)
 
     $month = str_pad($month, 2, "0", STR_PAD_LEFT);
 
+
     while ($currentDay <= $numberDays) {
 
         if ($dayOfWeek == 7) {
+
             $dayOfWeek = 0;
             $calendar .= "</tr><tr>";
         }
@@ -132,99 +139,34 @@ function build_calendar($month, $year)
         $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
         $date = "$year-$month-$currentDayRel";
 
+
         $today = $date == date('Y-m-d') ? "today" : "";
 
         if ($dayOfWeek == 5 || $dayOfWeek == 6) {
             $calendar .= "<td class='weekend'><h4>$currentDay</h4></td>";
         } elseif ($date < date('Y-m-d')) {
-            $calendar .= "<td class='crossed'><h4>$currentDay</h4>";
+            $calendar .= "<td class='crossed' ><h4>$currentDay</h4>";
         } elseif (in_array($date, $bookings)) {
-            $calendar .= "<td class='$today'><h4>$currentDay
-            // Display booked date with a link to booking details
-            
-
-            // Add any additional styling or information for booked dates
-            // For example, you can add a badge indicating the booking status
-            // $calendar .= "<span class='badge badge-primary'>Booked</span></h4>";
+            $calendar .= "<td class='$today'><h4>$currentDay</h4> <button class='btn btn-danger btn-xs'> <span class='glyphicon glyphicon-lock
+            '></span> Already Booked</button>";
         } else {
-            // Display available date for booking
-            $calendar .= "<td class='$today'><h4>$currentDay</h4>";
-
-            // Add a button to book the date
-            $calendar .= "<a href='booking.php?date=$date' class='btn btn-primary'>Book Now</a></td>";
+            $calendar .= "<td class='$today'><h4>$currentDay</h4> <a href='book.php?date=" . $date . "' class='btn btn-info btn-xs'> <span class='glyphicon glyphicon-ok'></span> Book Now</a>";
         }
 
         $calendar .= "</td>";
-
         $currentDay++;
         $dayOfWeek++;
     }
-
+ 
     if ($dayOfWeek != 7) {
+
         $remainingDays = 7 - $dayOfWeek;
-        for ($i = 0; $i < $remainingDays; $i++) {
+        for ($l = 0; $l < $remainingDays; $l++) {
             $calendar .= "<td class='empty'></td>";
         }
     }
 
     $calendar .= "</tr>";
     $calendar .= "</table>";
-
-    return $calendar;
+    echo $calendar;
 }
-
-?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Vehicle Inspection Booking Calendar</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .today {
-            background-color: #f0ad4e !important;
-        }
-
-        .crossed {
-            text-decoration: line-through;
-            color: #ccc;
-        }
-
-        .weekend {
-            background-color: #f7f7f7;
-        }
-
-        .empty {
-            background-color: #eaeaea;
-        }
-
-        .header {
-            background-color: #337ab7;
-            color: #fff;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="container">
-        <h1 class="display-4 text-center">Vehicle Inspection Booking Calendar</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <?php
-                $dateComponents = getdate();
-                if (isset($_GET['month']) && isset($_GET['year'])) {
-                    $month = $_GET['month'];
-                    $year = $_GET['year'];
-                } else {
-                    $month = $dateComponents['mon'];
-                    $year = $dateComponents['year'];
-                }
-                echo build_calendar($month, $year);
-                ?>
-            </div>
-        </div>
-    </div>
-</body>
-
-</html>
