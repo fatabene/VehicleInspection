f928426e0e67ee2deb821dff5e3d7085b7527064
fatabene <94798155+fatabene@users.noreply.github.com>
2023-05-11 17:57:28 +0200

    Update my_bookings.php

--- my_bookings.php
+++ my_bookings.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <title>Vehicle Inspection Booking System</title>
 </head>
+
 <body>
+    <div class="container-fluid p-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert" style="background: #17a2b8; color: #fff; border-color: #64e9f1;">
+                    <h1 class="display-1 text-center" style="font-size: 40px">Vehicle inspection Motor AB</h1>
+                </div>
       
 <?php
 $host = 'containers-us-west-145.railway.app';
@@ -26,7 +34,7 @@ $currentMonth = date('m');
 $query = "SELECT * FROM vehicleinspection_bookings_record WHERE MONTH(DATE) = '$currentMonth'";
 $result = $conn->query($query);
 ?>
-
+<div class="table-responsive">
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -74,5 +82,7 @@ $result = $conn->query($query);
             </div>
         </div>
     </div>
+     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>
