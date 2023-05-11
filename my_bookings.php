72d18fc28f3d267f27c4f4d39d1e0b7fc3132aae
fatabene <94798155+fatabene@users.noreply.github.com>
2023-05-11 17:44:38 +0200

    Update my_bookings.php

--- my_bookings.php
+++ my_bookings.php
@@ -1,19 +1,4 @@
-<html>
 
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
- 
-</head>
-
-<body>
-  <div class="container-fluid P-5">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="alert" style="background: #17a2b8; color: #fff; border-color: #64e9f1;">
-   
-          </div>
       
 <?php
 $host = 'containers-us-west-145.railway.app';
