115c16601a9a2585717c4dab5de3bef0cebc73f4
fatabene <94798155+fatabene@users.noreply.github.com>
2023-05-11 17:20:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
+<!DOCTYPE html>
 <html>
 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <title>Vehicle Inspection Booking System</title>
+  <style>
+    .jumbotron {
+      background: #17a2b8;
+      color: #fff;
+      border-color: #64e9f1;
+    }
+
+    .display-1 {
+      font-size: 40px;
+    }
+  </style>
 </head>
 
 <body>
-  <div class="container-fluid P-5">
+  <div class="container-fluid p-5">
     <div class="row">
       <div class="col-md-12">
-        <div class="alert" style="background: #17a2b8; color: #fff; border-color: #64e9f1;">
-          <h1 class="display-1 text-center" style="font-size: 40px">Vehicle inspection Motor AB</h1>
-          </div>
+        <div class="alert alert-primary">
+          <h1 class="display-4 text-center">Vehicle Inspection Motor AB</h1>
+        </div>
+        
 
           <?php
           include 'build.php';
