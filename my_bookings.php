f3c03f36e63dd07ccfc6522edc6a448a53a8fd5e
fatabene <94798155+fatabene@users.noreply.github.com>
2023-05-11 18:03:33 +0200

    Update my_bookings.php

--- my_bookings.php
+++ my_bookings.php
@@ -1,4 +1,21 @@
-      
+ <!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Vehicle Inspection Booking System</title>
+</head>
+
+<body>
+    <div class="container-fluid p-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert" style="background: #17a2b8; color: #fff; border-color: #64e9f1;">
+                    <h1 class="display-1 text-center" style="font-size: 40px">Vehicle inspection Motor AB</h1>
+                </div>
+            </div>
+        </div>     
 <?php
 $host = 'containers-us-west-145.railway.app';
 $port = '7013';
